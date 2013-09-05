1613765925a0bd0ad28b4d1522454e17b8e7dd92
Vedad Hodzic <vedadh@student.matnat.uio.no>
2013-09-05 16:18:02 +0200

    Algorithm working.
    Programmed a working example for the tridiagonal matrix algorithm. The
    data is written to a file, but have not plotted it yet. Don't know if
    it's the right result.
    Also made a header file.

---
+++ tdma.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include "tdma.h"
+
+using namespace std;
+
+
+void doAlgorithm(double aVals, double bVals, double cVals, int N) {
+
+    double a [N-1];
+    double b [N];
+    double c [N-1];
+    double f [N];
+    double v [N];
+    double x [N];
+
+    fill_n(a, N-1, aVals);
+    fill_n(b, N, bVals);
+    fill_n(c, N-1, cVals);
+
+    a[0] = 0.0;
+    c[N-2] = 0.0;
+
+    double h = 1./(N+1);
+
+    /* Forward substitution */
+
+    for (int i=0; i < N; i++) {
+
+        f[i] = h * h * (100 * exp(-10 * i * h));
+        b[i+1] -= c[i] * a[i+1] / b[i];
+        f[i+1] -= f[i] * a[i+1] / b[i];
+
+    }
+
+    /* Backward substitution */
+
+    for (int i=(N-1); i >= 0; i--) {
+
+        v[i] = (f[i] - c[i] * v[i+1]) / b[i];
+
+    }
+
+    fstream outFile;
+    outFile.open("linEqSolutionSet.txt", ios::out);
+
+    for (int i=0; i < N; i++) {
+
+        x[i] = i/(double)(N);
+        outFile << setprecision(3) << x[i] << " " << v[i] << endl;
+
+    }
+
+    outFile << endl;
+    outFile.close();
+
+}
---
+++ tdma.h
@@ -0,0 +1,4 @@
+#ifndef TDMA_H
+#define TDMA_H
+void doAlgorithm(double, double, double, int);
+#endif // TDMA_H
